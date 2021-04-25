af886efcccbeee0cf3db341d60c029efcbafea7c
knarf_ <francisco@getxplor.com>
2021-04-25 08:41:29 +0200

    It was added new fields into photo model

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 require('dotenv').config();
+
+const moment = require('moment-timezone');
 const Cloudinary = require('cloudinary').v2;
 Cloudinary.config({
     cloud_name: process.env.CLOUNDRY_NAME,
@@ -26,7 +28,7 @@ app.use(bodyParser.json({
 }));
 app.use(morgan('dev'));
 
-app.post('/storing_photos', async (req, res) => {
+app.post('/storing_photo', async (req, res) => {
     try {
         var params = req.body;
         Cloudinary.uploader.upload(params.img,
@@ -41,7 +43,12 @@ app.post('/storing_photos', async (req, res) => {
                 var img = result.secure_url;
                 var photo_model = new PhotoModel({
                     photo: img,
-                    detail_photo: params.detail_photo
+                    detail_item: params.detail_item,
+                    stole_item: params.stole_item,
+                    where_was_item: params.where_was_item,
+                    folder_number: params.folder_number,
+                    unit_number: params.unit_number,
+                    agency_number: params.agency_number,
                 });
                 var data = await photo_model.save();
                 if (data == null) {
@@ -65,9 +72,9 @@ app.post('/storing_photos', async (req, res) => {
     }
 });
 
-app.get('/list_stoting', async (req, res) => {
+app.get('/list_storing', async (req, res) => {
     try {
-        var data = await PhotoModel.find({}).lean();
+        var data = await PhotoModel.find({}).select('_id photo stole_item');
         if (data.length <= 0) {
             return res.status(200).json({
                 cd: 'Fail',
@@ -87,10 +94,10 @@ app.get('/list_stoting', async (req, res) => {
     }
 });
 
-app.get('/stroting_detail', async (req, res) => {
+app.get('/storiing_detail', async (req, res) => {
     try {
         var _id = req.query._id;
-        var data = await PhotoModel.findOne({ _id: _id }).select('detail_photo photo');
+        var data = await PhotoModel.findOne({ _id: _id }).lean();
         
         if(data == null) {
             return res.status(200).json({
--- photo.detail.model.js
+++ photo.detail.model.js
@@ -5,8 +5,13 @@ const Schema = mongoose.Schema;
 
 const PhotoDetailModel = Schema({
     photo: { type: String },
-    detail_photo: { type: String },
-    create_photo: { type: Date, default: new Date() }
+    stole_item: { type: String },
+    where_was_item: { type: String },
+    folder_number: { type: String },
+    unit_number: { type: String },
+    agency_number: { type: String },
+    detail_item: { type: String },
+    date_create_info: { type: Date, default: new Date() }
 });
 
 const PhotoDetails = mongoose.model('PhotoDetails', PhotoDetailModel);
