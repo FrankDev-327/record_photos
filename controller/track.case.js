dfdee924f4092b5f471c18b919b6966dbbf368a4
FrankDev-327 <salutacinasfer@hotmail.com>
2021-05-15 07:35:40 +0200

    make it better

--- controller/track.case.js
+++ controller/track.case.js
@@ -9,7 +9,7 @@ async function insertDataAboutCase(req, res) {
         params.img = 'data:image/jpeg;base64,' + params.img;
         Cloudinary.uploader.upload(params.img,
             async function (err, result) {
-                if (!err) {
+                if (err !== undefined) {
                     console.log(err)
                     return res.status(301).json({
                         cd: 'No Success!',
