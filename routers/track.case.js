da1675bd6c188d268c9fa9d844f0b4e1c9a0ad55
FrankDev-327 <salutacinasfer@hotmail.com>
2021-05-15 07:20:01 +0200

    make it better

--- routers/track.case.js
+++ routers/track.case.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { TrackCaseController } = require('../controller/index');
-
+//--
 router.post('/case', TrackCaseController.insertDataAboutCase);
 router.get('/cases', TrackCaseController.listsDataAboutCases);
 router.get('/case/:_id', TrackCaseController.caseInformation);
